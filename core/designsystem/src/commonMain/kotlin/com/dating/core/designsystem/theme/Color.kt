6038fbd38ffe86d8f0d35cbdd65bb448ed0157fc
EdgarBarcenas1597 <barcenased@globalhitss.com>
2026-01-28 20:21:40 -0600

    Fix Resources

--- core/designsystem/src/commonMain/kotlin/com/dating/core/designsystem/theme/Color.kt
+++ core/designsystem/src/commonMain/kotlin/com/dating/core/designsystem/theme/Color.kt
@@ -2,70 +2,64 @@ package com.dating.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val ChirpBrand1000 = Color(0xFFEDE6F5) // Lavanda muy claro
-val ChirpBrand900 = Color(0xFF2B0A3D) // Fondo header oscuro
-val ChirpBrand600 = Color(0xFF6A1FA1)
-val ChirpBrand500 = Color(0xFF9B5CFF) // Primary / CTA
-val ChirpBrand500Alpha40 = Color(0x669B5CFF)
-val ChirpBrand100 = Color(0xFFE9D9FF) // Containers claros
+// Brand
+val ChirpBrand500 = Color(0xFFFF5A7A) // Primary
+val ChirpBrand600 = Color(0xFFE84A68) // Hover / pressed
+val ChirpBrand100 = Color(0xFFFFE3E8) // Containers
+val ChirpBrand900 = Color(0xFF4A1D2A) // Dark containers
+val ChirpBrand1000 = Color(0xFFFFFFFF) // Texto sobre brand
 
-// Base Colors
-val ChirpBase1000 = Color(0xFF0E0714) // Background dark principal
-val ChirpBase1000Alpha8 = Color(0x140E0714)
-val ChirpBase1000Alpha80 = Color(0xCC0E0714)
-val ChirpBase950 = Color(0xFF160B22)
-val ChirpBase900 = Color(0xFF221133)
-val ChirpBase800 = Color(0xFF352047)
-val ChirpBase700 = Color(0xFF4D3A61)
-val ChirpBase500 = Color(0xFF7B6D8F)
-val ChirpBase400 = Color(0xFF9A8CAD)
-val ChirpBase200 = Color(0xFFD9D2E5)
-val ChirpBase150 = Color(0xFFE6E0EF)
-val ChirpBase100 = Color(0xFFF4F1FA)
-val ChirpBase100Alpha10 = Color(0x1AF4F1FA)
-val ChirpBase1000Alpha14 = Color(0x240E0714)
+// Base Light
 val ChirpBase0 = Color(0xFFFFFFFF)
+val ChirpBase50 = Color(0xFFF9F9FB)
+val ChirpBase100 = Color(0xFFF1F1F5)
+val ChirpBase200 = Color(0xFFE1E1E8)
+val ChirpBase700 = Color(0xFF6B6B76)
+val ChirpBase900 = Color(0xFF2C2C34)
 
-// Red Colors
-val ChirpRed600 = Color(0xFFB0123A)
-val ChirpRed500 = Color(0xFFE63964)
-val ChirpRed200 = Color(0xFFFFB3C2)
+// Base Dark
+val ChirpBase1000 = Color(0xFF120D14)
+val ChirpBase950 = Color(0xFF1A131D)
+val ChirpBase900D = Color(0xFF241B28)
+val ChirpBase400D = Color(0xFFB0AEB8)
 
-// Accent Colors (15% alpha)
-val ChirpBlue = Color(0xFF1DA1F2)
-val ChirpBlue2 = Color(0xFF7FCBFF)
-val ChirpBlue3 = Color(0xFF7FCBFF)
+// Feedback
+val ChirpRed500 = Color(0xFFE53935)
+val ChirpRed600 = Color(0xFFC62828)
+val ChirpRed200 = Color(0xFFFFCDD2)
 
-val ChirpPurple = Color(0x269B5CFF)
-val ChirpViolet = Color(0x26C77DFF)
-val ChirpPink = Color(0x26FF8FB1)
-val ChirpOrange = Color(0x26FFB37A)
-val ChirpYellow = Color(0x26FFE89A)
-val ChirpGreen = Color(0x2686FFC2)
-val ChirpTeal = Color(0x2674FFE5)
-val ChirpLightBlue = Color(0x268EC5FF)
-val ChirpGrey = Color(0x26D9D2E5)
+// Cake Colors – Light Theme
+val ChirpCakeLightViolet = Color(0xFFF2E8FF)
+val ChirpCakeLightPurple = Color(0xFFEAD9FF)
+val ChirpCakeLightPink = Color(0xFFFFE3EB)
+val ChirpCakeLightRed = Color(0xFFFFE1E6)
 
-// Cake Colors - Light Theme (Soft pastel shades with good contrast)
-val ChirpCakeLightViolet = Color(0xFFEFE2FF)
-val ChirpCakeLightGreen = Color(0xFFE2FFF2)
-val ChirpCakeLightBlue = Color(0xFFE2EEFF)
-val ChirpCakeLightPink = Color(0xFFFFE2EC)
-val ChirpCakeLightOrange = Color(0xFFFFEFE2)
-val ChirpCakeLightYellow = Color(0xFFFFFAE2)
-val ChirpCakeLightTeal = Color(0xFFE2FFF8)
-val ChirpCakeLightPurple = Color(0xFFE8DEFF)
-val ChirpCakeLightRed = Color(0xFFFFE2E6)
-val ChirpCakeLightMint = Color(0xFFE2FFF0)
+val ChirpCakeLightBlue = Color(0xFFE6F0FF)
+val ChirpCakeLightTeal = Color(0xFFE3F7F5)
+val ChirpCakeLightGreen = Color(0xFFE6F7EC)
+val ChirpCakeLightMint = Color(0xFFE4FAF2)
 
-// Cake Colors - Dark Theme (Muted shades with good contrast)
-val ChirpCakeDarkViolet = Color(0x269B5CFF)
-val ChirpCakeDarkGreen = Color(0x2686FFC2)
-val ChirpCakeDarkBlue = Color(0x264F9DFF)
-val ChirpCakeDarkPink = Color(0x26FF8FB1)
-val ChirpCakeDarkOrange = Color(0x26FFB37A)
-val ChirpCakeDarkYellow = Color(0x26FFE89A)
-val ChirpCakeDarkTeal = Color(0x2674FFE5)
-val ChirpCakeDarkPurple = Color(0x26C77DFF)
-val ChirpCakeDarkRed = Color(0x26FF6B8A)
-val ChirpCakeDarkMint = Color(0x2686FFD4)
+val ChirpCakeLightOrange = Color(0xFFFFF0E3)
+val ChirpCakeLightYellow = Color(0xFFFFF8E1)
+
+
+// Cake Colors – Dark Theme
+val ChirpCakeDarkViolet = Color(0xFF3B1E52)
+val ChirpCakeDarkPurple = Color(0xFF46215F)
+val ChirpCakeDarkPink = Color(0xFF5A2438)
+val ChirpCakeDarkRed = Color(0xFF61232D)
+
+val ChirpCakeDarkBlue = Color(0xFF1E2E4F)
+val ChirpCakeDarkTeal = Color(0xFF1E3F3B)
+val ChirpCakeDarkGreen = Color(0xFF1F3A2C)
+val ChirpCakeDarkMint = Color(0xFF1E4237)
+
+val ChirpCakeDarkOrange = Color(0xFF4A2E1E)
+val ChirpCakeDarkYellow = Color(0xFF4A3F1E)
+
+// Accent – Premium / VIP / New
+val AccentPremium = Color(0xFF4BB0FF) // Azul premium
+val AccentPremiumDark = Color(0xFF6BC1FF)
+
+val AccentVip = Color(0xFFFFC83D) // Amarillo VIP
+val AccentVipDark = Color(0xFFFFD666)
